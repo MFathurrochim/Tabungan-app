5547fbc3b61e5c45b7292834755bf80f0f8c065e
MFathurrochim <faturohimskd06@gmail.com>
2025-04-09 22:14:14 +0700

    memperbarui kode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#5D5CDE',
+        secondary: '#4CAF50',
+        success: '#4CAF50',
+        danger: '#FF5252',
+        warning: '#FFC107',
+        dark: {
+          DEFAULT: '#181818',
+          light: '#2d2d2d'
+        }
+      }
+    },
   },
   plugins: [],
 }
