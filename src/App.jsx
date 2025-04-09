052ee8116d4b595a06236468cecc1ab6fb5da2e9
MFathurrochim <faturohimskd06@gmail.com>
2025-04-09 22:08:49 +0700

    membuat app.js nya

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-import { useState } from 'react'
-import Home from './pages/Home'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AppProvider } from './contexts/AppContext';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
+import TransaksiPage from './pages/TransaksiPage';
+import TargetPage from './pages/TargetPage';
+import JadwalPage from './pages/JadwalPage';
+import StatistikPage from './pages/Statistik';
 
 function App() {
-  return <Home />
+  return (
+    <AppProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/transaksi" element={<TransaksiPage />} />
+            <Route path="/target" element={<TargetPage />} />
+            <Route path="/jadwal" element={<JadwalPage />} />
+            <Route path="/statistik" element={<StatistikPage />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </AppProvider>
+  );
 }
 
-export default App
+export default App;
